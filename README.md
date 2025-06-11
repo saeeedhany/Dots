bee49218dfb3b16fd091ce41c08f67f10bb4cdc6
saeeedhany <alsaeedalbasi0ny@gmail.com>
2025-06-11 23:04:47 +0300

    Making it proficinal

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Dots
+# ThePrimeSetup.conf 
 This Repo just have my environemnt config files
---
+++ gruveed/nvim/init.lua
@@ -0,0 +1,6 @@
+vim.opt.termguicolors = true
+
+require('core.options') 
+require('core.keymaps')
+require('plugins')
+print("Neovim config loaded!") 
